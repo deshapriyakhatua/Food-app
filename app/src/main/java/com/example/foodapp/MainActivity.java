97edaadcd4f6619f3736f3ebe0ea07cbca25ce17
deshapriya <deshapriyakhatua@gmail.com>
2023-01-17 19:07:42 +0530

    17 01 2023

--- app/src/main/java/com/example/foodapp/MainActivity.java
+++ app/src/main/java/com/example/foodapp/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
 
+
         setSupportActionBar(binding.appBarMain.toolbar);
         binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
             @Override
